64addb23cfc48a910758f12ffec75b318a7eaccf
julian kiefer <juliankiefer@julians-MBP.fritz.box>
2020-05-21 18:22:29 +0200

    laufen

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const hostname = 'localhost';
 let port = 8080;
+var path = require('path');
 
-app.get('/', (req, res) => res.send('lololol'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../', 'index.html')));
 
 if (process.argv[2] !== undefined) {
   console.log('Custom Argument');
