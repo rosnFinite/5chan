50c6155799714f5ffa3185edf692b6f8ca7158e1
julian kiefer <juliankiefer@julians-MBP.fritz.box>
2020-05-22 13:35:27 +0200

    TestCommonJsModules

---
+++ src/js/articles.js
@@ -0,0 +1,11 @@
+const testPosts = [{
+  title: 'Test article',
+  timestamp: new Date(),
+  post: 'hier steht was'
+},
+{
+  title: 'Test article2',
+  timestamp: new Date(),
+  post: 'hier steht wasneues'
+}];
+exports.testPosts = testPosts;
