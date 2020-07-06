bbddf4786178c58d405af610e9c80ce76f93dbdb
julian kiefer <juliankiefer@julians-MBP.fritz.box>
2020-07-06 15:34:16 +0200

    Seitenzahlen

--- src/js/clientside.js
+++ src/js/clientside.js
@@ -3,6 +3,7 @@ const createPost = require('./createpost.js');
 const createForm = require('./newPostForm.js');
 const mime = require('mime-types');
 var siteCounter = 0;
+var maxSites = 0;
 
 function extension (filePath) {
   return mime.lookup(filePath);
@@ -46,7 +47,12 @@ function extension (filePath) {
 
         var postCounterThisSite = 0;
         var postCounter = xhr.response.data.length - 1;
-        console.log('es lüuft weidejoidj');
+        maxSites = Math.ceil(postCounter / 10);
+        var aktSite = (siteCounter / 10) + 1;
+        console.log(aktSite);
+        document.getElementById('siteNumber').innerHTML = 'Seiten: ' + maxSites;
+        document.getElementById('siteNumberAkt').innerHTML = 'Akt Seite: ' + aktSite;
+
         createPost.removeAllPosts();
         postCounter -= siteCounter;
         while (postCounter >= 0) {
@@ -89,8 +95,10 @@ function extension (filePath) {
   });
   nextSite.addEventListener('click', function (e) {
     // muss noch gecaped werden
-    siteCounter += 10;
-    document.getElementById('retrieve').click();
+    if (siteCounter / 10 + 1 < maxSites) {
+      siteCounter += 10;
+      document.getElementById('retrieve').click();
+    }
   });
   prevSite.addEventListener('click', function (e) {
     if (siteCounter > 0) {
